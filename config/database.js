7c77874c345cab8e5f53655ea9616b784e07e37c
mostafa ragap <mostafaragap350@gmail.com>
2023-06-18 21:56:01 +0300

    change database

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 //CONNECT TO DB
 const DBconnection = () => {
-  mongoose.connect("mongodb+srv://nutriGym:lHQjk74v4caju5gM@cluster0.15cjlmc.mongodb.net/s").then((conn) => {
+  mongoose.connect("mongodb+srv://nutriGym:lHQjk74v4caju5gM@cluster0.15cjlmc.mongodb.net/test").then((conn) => {
     console.log(`DataBase connected ${conn.connection.host}`);
   });
   // .catch((err) => {
