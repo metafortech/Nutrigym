749aa6aa306c1c93f645a32b7eea63d1d96669b5
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-06-21 21:38:00 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,33 +6,52 @@ const ApiErr = require("../utils/apiError");
 const generateToken = require("../utils/createToken");
 
 const { uploadSingleImage } = require("../middlewares/uploadImageMiddleware");
-
+var FCM = require("fcm-node");
 const Factory = require("./handlerFactory");
+
 const Club = require("../models/clubModel");
 const Clinic = require("../models/clinicModel");
 const Phyclinic = require("../models/phyiscalclinicModel");
 
 const User = require("../models/userModel");
 
-///////////////////////////////////////////////////////
+const admin = require("firebase-admin");
+
+async function getAllDeviceTokens() {
+  try {
+    const users = await User.find();
+    const tokens = users.map((user) => user.deviceToken);
+    return tokens;
+  } catch (error) {
+    console.error("Error fetching device tokens:", error);
+    throw error;
+  }
+}
+
+async function getDeviceTokenForUser(userId) {
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new Error("User not found.");
+    }
+    return user.deviceToken;
+  } catch (error) {
+    console.error("Error fetching device token for user:", error);
+    throw error;
+  }
+}
+
+
+const serviceAccount = require("../config/push-notification-key.json"); 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 module.exports = {
   createUser: Factory.createOne(User),
-  //@desc     create User
-  //route     POST /api/v1/Users
-  //access    private
-
   getUsers: Factory.getAll(User),
-  //@desc     get list of User
-  //route     GET /api/v1/Users
-  //access    private
-
   getUser: Factory.getOnebyId(User, "User", "subscibtions"),
-
-  //@desc     get specific User by id
-  //route     GET /api/v1/Users/:id
-  //access    private
-
   updateUser: asyncHandler(async (req, res, next) => {
     const document = await User.findByIdAndUpdate(
       req.params.id,
@@ -51,10 +70,6 @@ module.exports = {
 
     res.status(200).json({ data: document });
   }),
-  //@desc    update specific User by id
-  //route     PUT /api/v1/Users/:id
-  //access    private
-
   updateUserPassword: asyncHandler(async (req, res, next) => {
     const document = await User.findByIdAndUpdate(
       req.params.id,
@@ -72,25 +87,12 @@ module.exports = {
 
     res.status(200).json({ data: document });
   }),
-  //@desc    update specific UserPassword by id
-  //route     PUT /api/v1/Users/:id
-  //access    private
-
   deleteUser: Factory.deleteOne(User, "User"),
-  //@desc    delete specific User by id
-  //route     DELETE /api/v1/Users/:id
-  //access    private
-
   getLoggedUserData: asyncHandler(async (req, res, next) => {
     req.params.id = req.user._id;
     next();
   }),
-  //@desc     get logged user data
-  //route     GET /api/v1/Users/getMyData
-  //access    private/protect
-
   updateLoggedUserPassword: asyncHandler(async (req, res, next) => {
-    //1)update logged user passwored based on payload(user._id) from protect route
     const user = await User.findByIdAndUpdate(
       req.user._id,
       {
@@ -105,14 +107,12 @@ module.exports = {
       return next(new ApiErr(`no user with this id`, 404));
     }
 
-    //2) generate token
     const token = generateToken(user._id);
+    user.deviceToken = token;
+    await user.save();
 
     res.status(200).json({ data: user, token });
   }),
-  //@desc    update specific loggedUserPassword by id  user_id
-  //route     PUT /api/v1/Users/updateMyPassword
-  //access    private/Protect
   updateLoggedUserData: asyncHandler(async (req, res, next) => {
     const updatedUser = await User.findByIdAndUpdate(
       req.user._id,
@@ -127,19 +127,14 @@ module.exports = {
       .status(200)
       .json({ message: "Data Updated Succesfully", data: updatedUser });
   }),
-  //@desc    update specific LoggedUserData by id   user_id  (without password , role)
-  //route     PUT /api/v1/Users/updateMyData
-  //access    private/Protect
   addManagerToClub: asyncHandler(async (req, res, next) => {
     const { clubId, managerId } = req.body;
 
-    // Check if the club exists
     const club = await Club.findById(clubId);
     if (!club) {
       return res.status(404).json({ message: "Club not found" });
     }
 
-    // Check if the user exists and has the "manager" role
     const manager = await User.findById(managerId);
     if (!manager || manager.role !== "manager") {
       return res.status(404).json({ message: "Invalid manager" });
@@ -155,13 +150,11 @@ module.exports = {
   addManagerToClinic: asyncHandler(async (req, res, next) => {
     const { clinicId, managerId } = req.body;
 
-    // Check if the clinic exists
     const clinic = await Clinic.findById(clinicId);
     if (!clinic) {
       return res.status(404).json({ message: "Clinic not found" });
     }
 
-    // Check if the user exists and has the "manager" role
     const manager = await User.findById(managerId);
     if (!manager || manager.role !== "manager") {
       return res.status(404).json({ message: "Invalid manager" });
@@ -177,22 +170,15 @@ module.exports = {
   removeManagerFromClinic: asyncHandler(async (req, res, next) => {
     const { clinicId, managerId } = req.body;
 
-    // Check if the clinic exists
     const clinic = await Clinic.findById(clinicId);
     if (!clinic) {
       return res.status(404).json({ message: "Clinic not found" });
     }
 
-    // Check if the user exists and has the "manager" role
-    const manager = await User.findById(managerId);
-    if (!manager || manager.role !== "manager") {
-      return res.status(404).json({ message: "Invalid manager" });
-    }
-
-    clinic.manager = managerId;
+    clinic.manager = null;
     await clinic.save();
 
-    res.status(200).json({ message: "Manager added successfully" });
+    res.status(200).json({ message: "Manager removed successfully" });
   }),
   addManagerToPhyclinic: asyncHandler(async (req, res, next) => {
     const { phyClinicId, managerId } = req.body;
@@ -211,6 +197,60 @@ module.exports = {
     await phyClinic.save();
     manager.phyClinicManage.push(phyClinic._id);
     await manager.save();
+
     res.status(200).json({ message: "Manager added successfully" });
+  }),
+  sendNotificationToAllUsers: asyncHandler(async (req, res, next) => {
+   const SERVER_KEY = "AAAAaBC5ZYg:APA91bHxB0BdbkYQhugxZSM_NksbN2VK2PPLfFY9HcyiywpKYmsVaKU1K9VKSb3cQNxUzL5g8o0hFIFRZ9-nggKP5kL8tN-gCNEqn8iM0jPsE2SASzJj4CSVI2JVro5VdO0m6gJM2t8z"
+    let fcm = new FCM(SERVER_KEY);
+    const { title, body } = req.body;
+    const tokens = await getAllDeviceTokens(); // Implement your logic to fetch device tokens from the database
+    const message = {
+      to: "/topics/" + req.body.topic,
+      notification: {
+        title: title,
+        body: body,
+        sound: "default",
+        click_action: "FCM_PLUGIN_ACTIVITY",
+        icon: "fcm_push_icon",
+      },
+      tokens,
+    };
+    fcm.send(message, (err, response) => {
+      if (err) {
+        next(err);
+      } else {
+        res.json({ message: "Notification sent to all users.", response });
+      }
+    });
+
+  
+  }),
+  sendNotificationToUser: asyncHandler(async (req, res, next) => {
+   const SERVER_KEY = "AAAAaBC5ZYg:APA91bHxB0BdbkYQhugxZSM_NksbN2VK2PPLfFY9HcyiywpKYmsVaKU1K9VKSb3cQNxUzL5g8o0hFIFRZ9-nggKP5kL8tN-gCNEqn8iM0jPsE2SASzJj4CSVI2JVro5VdO0m6gJM2t8z"
+    let fcm = new FCM(SERVER_KEY);
+    const { userId } = req.params;
+    const { title, body } = req.body;
+    const token = await getDeviceTokenForUser(userId);
+    const message = {
+      to: "/topics/" + req.body.topic,
+      notification: {
+        title: title,
+        body: body,
+        sound: "default",
+        click_action: "FCM_PLUGIN_ACTIVITY",
+        icon: "fcm_push_icon",
+      },
+      token,
+    };
+    fcm.send(message, (err, response) => {
+      if (err) {
+        next(err);
+      } else {
+        res.json({ message: "Notification sent to this user.", response });
+      }
+    });
+
+ 
   }),
 };
