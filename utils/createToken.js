edb76d6ac23e08bcd26f55cb70bcd4a78dfc1c1c
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-06-27 16:21:30 +0300

    Delete utils directory

--- utils/createToken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (payload) =>
-  jwt.sign({ userId: payload }, "qawsedrftgyhujikol", {
-    expiresIn: "90d",
-  });
-
-module.exports = generateToken;
