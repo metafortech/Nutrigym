70ddc1f7256b4b5e6091530ae67fff5c1e3bc0b2
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-06-27 16:20:04 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,1 @@
-const mongoose = require("mongoose");
 
-//CONNECT TO DB
-const DBconnection = () => {
-  mongoose.connect("mongodb+srv://nutriGym:lHQjk74v4caju5gM@cluster0.15cjlmc.mongodb.net/test").then((conn) => {
-    console.log(`DataBase connected ${conn.connection.host}`);
-  });
-  // .catch((err) => {
-  //   console.error(`DataBase Error ${err}`);
-  //   process.exit(1);
-  // });
-};
-
-module.exports = DBconnection;
